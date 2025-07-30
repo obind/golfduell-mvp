9f57f3324f9734e77890b6d7b4831a63f059ed5b
Kenneth Ballen <obindob@gmail.com>
2025-07-30 09:23:47 -0700

    Create main.ts

---
+++ src/main.ts
@@ -0,0 +1,31 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import { createVuetify } from 'vuetify'
+import 'vuetify/styles'
+import { aliases, mdi } from 'vuetify/iconsets/mdi'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
+import { createPinia } from 'pinia'
+import router from './router'
+
+// Create Vuetify instance with default material design icons
+const vuetify = createVuetify({
+  components,
+  directives,
+  icons: {
+    defaultSet: 'mdi',
+    aliases,
+    sets: {
+      mdi,
+    },
+  },
+})
+
+const app = createApp(App)
+
+app.use(vuetify)
+app.use(createPinia())
+app.use(router)
+
+app.mount('#app')
