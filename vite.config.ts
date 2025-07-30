93871a40ca89ae645082dd74a87cfcd16a60dab2
obind <obind@example.com>
2025-07-30 16:46:52 -0400

    Fix: Build issues and update module imports, add .gitignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
-import { resolve } from 'path'
+import { fileURLToPath } from 'url'
+import path from 'path'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -24,7 +25,8 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      '@': resolve(__dirname, 'src'),
+      '@': path.resolve(path.dirname(fileURLToPath(import.meta.url)), 'src'),
     },
   },
 })
+
